fca1dd39092ccea7cee2528f69f080361ac25292
slimcherif <33696535+slimcherif@users.noreply.github.com>
2017-11-16 20:08:51 +0100

    Add files via upload

---
+++ ruby.rb
@@ -0,0 +1,19 @@
+
+def chiffre_de_cesar(string, shift = 5)
+ alphabet  = Array('a'..'z')
+  non_caps  = Hash[alphabet.zip(alphabet.rotate(shift))]
+  
+  alphabet = Array('A'..'Z')
+  caps     = Hash[alphabet.zip(alphabet.rotate(shift))]
+
+  alphabet = Array('!'..'?')
+  symbol     = Hash[alphabet.zip(alphabet.rotate(shift))]
+
+   encrypter = symbol.merge(non_caps.merge(caps))
+ 
+  string.chars.map { |c| encrypter.fetch(c, c) }
+end
+
+text = gets.chomp
+
+p chiffre_de_cesar("Alexia et Sebatien!").join
